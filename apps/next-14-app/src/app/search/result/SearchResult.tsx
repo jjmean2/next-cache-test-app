62355b4986f09636b027b6bb679f952572e781d5
Jongwan Lee <jjmean2@gmail.com>
2025-04-29 06:42:44 +0900

    chore: add next-14-app

---
+++ apps/next-14-app/src/app/search/result/SearchResult.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { ReactElement } from "react";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  page?: number;
+};
+export default function SearchResult(props: Props): ReactElement {
+  const { page = 1 } = props;
+  const router = useRouter();
+  console.log("🎨 render SearchResult Client component", page);
+  const { data, dataUpdatedAt, refetch } = useSuspenseQuery({
+    queryKey: ["search", "result", page],
+    queryFn: async () => {
+      console.log(
+        `🔍 [${new Date().toString()}] querying search result for ${page}...`
+      );
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${page}`
+      );
+      const data = await response.json();
+      return data;
+    },
+  });
+  console.log("📦 data", data);
+  console.log(
+    "⏰ %cdataUpdatedAt %c%s",
+    "color:red",
+    "color:yellow",
+    new Date(dataUpdatedAt).toString()
+  );
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          const page = Number(formData.get("page"));
+          router.push("/search/result?page=" + page);
+        }}
+      >
+        <legend>Client 이동</legend>
+        <input
+          className="text-black"
+          type="number"
+          name="page"
+          defaultValue={page}
+        />
+        <button>페이지 이동</button>
+      </form>
+      <div>Page: {page}</div>
+      <button onClick={() => refetch()}>refetch</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
